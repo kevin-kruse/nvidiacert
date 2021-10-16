0ec3b0513c57feb919ac20c938cce414ab44bdc1
Kevin Kruse <kevin.kruse@rwth-aachen.de>
2021-10-16 11:49:28 -0700

    First steps

--- my-detection.py
+++ my-detection.py
@@ -8,7 +8,12 @@ display = jetson.utils.videoOutput("rtp://192.168.86.42:1234","--headless") # 'm
 while True:
     img = camera.Capture()
     detections = net.Detect(img)
-    print(detections)
+    objects = ["nothing"]
+    for detection in detections:
+        objects.append(net.GetClassDesc(detection.ClassID))
+    if "person" in objects:
+        print("Person detected!")
+    print(objects)
     display.Render(img)
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
     if not camera.IsStreaming() or not display.IsStreaming():
